26d9e2c8e91cf2e4af9f978b34315d981705fb26
YUIJZEON <dingyujen@gmail.com>
2021-08-22 06:12:57 +0800

    Add files via GitHub Desktop

---
+++ branch/UTAUotoReplacer/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UTAUotoReplacer
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            ReadRule("Replace.conf");
+        }
+
+        private void ReadRule(string RuleTittle)
+        {
+            TextReader txtReader = new StreamReader(RuleTittle);
+            while (txtReader.Peek() != -1)
+            {
+                string[] parts = txtReader.ReadLine().Split('\t');
+                dataGridView2.Rows.Add(parts);
+            }
+            txtReader.Close();
+        }
+
+        private void ReadOto(string otoTittle)
+        {
+            TextReader txtReader = new StreamReader(otoTittle);
+            while (txtReader.Peek() != -1)
+            {
+                string[] parts = txtReader.ReadLine().Split('=', ',');
+                dataGridView1.Rows.Add(parts);
+            }
+            txtReader.Close();
+        }
+
+        private void WriteOto(string otoTittle)
+        {
+            string s = ""; //新建一ㄍ空字串
+            int datarows = dataGridView1.Rows.Count - 1; //有 n 列資料
+            int counter = 0; //While迴圈計數器
+            while (counter < datarows)
+            {
+                s += GetDataGrid(0, counter);
+                s += "=";
+                s += GetDataGrid(1, counter);
+                s += ",";
+                s += GetDataGrid(2, counter);
+                s += ",";
+                s += GetDataGrid(3, counter);
+                s += ",";
+                s += GetDataGrid(4, counter);
+                s += ",";
+                s += GetDataGrid(5, counter);
+                s += ",";
+                s += GetDataGrid(6, counter);
+                s += "\r\n";
+                counter = counter + 1;
+            }
+            StreamWriter sw = new StreamWriter(otoTittle);
+            sw.Write(s);
+            sw.Close();
+            MessageBox.Show(s, "oto Saved!");
+        }
+
+        private string GetDataGrid(int x, int y)
+        {
+            if (dataGridView1[x, y].Value != null)
+            {
+                return dataGridView1[x, y].Value.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private string GetDataGrid2(int x, int y)
+        {
+            if (dataGridView2[x, y].Value != null)
+            {
+                return dataGridView2[x, y].Value.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private void AddDataGrid(string s, int x, int y)
+        {
+            if (dataGridView1[x, y].Value != null)
+            {
+                s += dataGridView1[x, y].Value.ToString();
+            }
+        }
+
+        private void GetOldValueLength()
+        {
+            int counter = 0;
+            int replaceRows = dataGridView2.Rows.Count - 1;
+            while (counter < replaceRows)
+            {
+                string s = GetDataGrid2(0, counter);
+                int n = s.Length;
+                dataGridView2[2, counter].Value = n;
+                counter = counter + 1;
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Initial File|*.ini|All Files(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.Rows.Clear();
+                ReadOto(openFileDialog.FileName.ToString());
+            }
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "oto";
+            saveFileDialog.Filter = "Initial File|*.ini|All Files(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                WriteOto(saveFileDialog.FileName.ToString());
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void replaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetOldValueLength();
+            dataGridView2.Sort(dataGridView2.Columns[2], System.ComponentModel.ListSortDirection.Descending);
+            int counter = 0;
+            int otoRows = dataGridView1.Rows.Count - 1; //有 n 列 oto
+            while (counter < otoRows)
+            {
+                string s = GetDataGrid(1, counter);
+                int counter2 = 0;
+                int replaceRows = dataGridView2.Rows.Count - 1; //有 n 列取代條件
+                while (counter2 < replaceRows)
+                {
+                    s = s.Replace(GetDataGrid2(0, counter2), GetDataGrid2(1, counter2));
+                    dataGridView1[1, counter].Value = s;
+                    counter2 = counter2 + 1;
+                }
+                dataGridView1[1, counter].Value = s;
+                counter = counter + 1;
+            }
+            MessageBox.Show("Finished!");
+        }
+    }
+}
---
+++ branch/UTAUotoReplacer_Backup/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace UTAUotoReplacer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.replaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Alias = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Offset = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Consonant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cutoff = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Preutterance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Overlap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.OldValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NewValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(743, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.saveToolStripMenuItem.Text = "Save As...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(123, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.replaceToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // replaceToolStripMenuItem
+            // 
+            this.replaceToolStripMenuItem.Name = "replaceToolStripMenuItem";
+            this.replaceToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.replaceToolStripMenuItem.Text = "Replace";
+            this.replaceToolStripMenuItem.Click += new System.EventHandler(this.replaceToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
+            this.Alias,
+            this.Offset,
+            this.Consonant,
+            this.Cutoff,
+            this.Preutterance,
+            this.Overlap});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(743, 237);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // FileName
+            // 
+            this.FileName.HeaderText = "Name";
+            this.FileName.Name = "FileName";
+            // 
+            // Alias
+            // 
+            this.Alias.HeaderText = "Alias";
+            this.Alias.Name = "Alias";
+            // 
+            // Offset
+            // 
+            this.Offset.HeaderText = "Offset";
+            this.Offset.Name = "Offset";
+            // 
+            // Consonant
+            // 
+            this.Consonant.HeaderText = "Consonant";
+            this.Consonant.Name = "Consonant";
+            // 
+            // Cutoff
+            // 
+            this.Cutoff.HeaderText = "Cutoff";
+            this.Cutoff.Name = "Cutoff";
+            // 
+            // Preutterance
+            // 
+            this.Preutterance.HeaderText = "Preutterance";
+            this.Preutterance.Name = "Preutterance";
+            // 
+            // Overlap
+            // 
+            this.Overlap.HeaderText = "Overlap";
+            this.Overlap.Name = "Overlap";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OldValue,
+            this.NewValue});
+            this.dataGridView2.Location = new System.Drawing.Point(491, 99);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // OldValue
+            // 
+            this.OldValue.HeaderText = "OldValue";
+            this.OldValue.Name = "OldValue";
+            // 
+            // NewValue
+            // 
+            this.NewValue.HeaderText = "NewValue";
+            this.NewValue.Name = "NewValue";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 261);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "oto Alias Replacer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem replaceToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OldValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NewValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Alias;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Offset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Consonant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cutoff;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Preutterance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Overlap;
+    }
+}
+
